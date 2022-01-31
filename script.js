193b8eeb72cc57e4ffaac450014e2e3b600f1807
Oleg <dobrunja90@yahoo.ca>
2022-01-30 21:10:19 -0500

    fixing time issue

--- script.js
+++ script.js
@@ -44,6 +44,8 @@ $(document).ready(function () {
     }
 
     // Request from local storage
+    $("#hour6 .description").val(localStorage.getItem("hour6"));
+    $("#hour7 .description").val(localStorage.getItem("hour7"));
     $("#hour8 .description").val(localStorage.getItem("hour8"));
     $("#hour9 .description").val(localStorage.getItem("hour9"));
     $("#hour10 .description").val(localStorage.getItem("hour10"));
@@ -54,6 +56,10 @@ $(document).ready(function () {
     $("#hour15 .description").val(localStorage.getItem("hour15"));
     $("#hour16 .description").val(localStorage.getItem("hour16"));
     $("#hour17 .description").val(localStorage.getItem("hour17"));
-
+    $("#hour18 .description").val(localStorage.getItem("hour18"));
+    $("#hour19 .description").val(localStorage.getItem("hour19"));
+    $("#hour20 .description").val(localStorage.getItem("hour20"));
+    $("#hour21 .description").val(localStorage.getItem("hour21"));
+    $("#hour22 .description").val(localStorage.getItem("hour22"));
     timeNowHr();
 })
